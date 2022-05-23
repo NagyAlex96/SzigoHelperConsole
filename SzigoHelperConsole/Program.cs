44f728f556e41381841feb89a5a217c20cbcf97f
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-05-23 18:51:49 +0200

    Rendezések (egyszerű, buborék, minkiválasztásos, beillesztéses)

--- SzigoHelperConsole/Program.cs
+++ SzigoHelperConsole/Program.cs
@@ -7,21 +7,24 @@ namespace SzigoHelperConsole
         static void Main(string[] args)
         {
             #region Segítség a Turple-höz és a lambdához
-            int[] tomb = { 1, 2, 3, 4, 9, 8, 7, 6, 5, 10 };
 
-            bool van = false;
-            int? index = null;
+            //int[] tomb = { 1, 2, 3, 4, 9, 8, 7, 6, 5, 10 };
 
-            (van, index) = EgyszeruProgTetelek.LinearisKeresesTetel(tomb, labda => tomb[labda] % 9 == 0); //fontos az igaz állítás, mivel a feltétel !P(x[i]) 
-            Console.WriteLine($"Van ilyen elem: {van}\nKeresett eleme indexe: {index}");
+            //bool van = false;
+            //int? index = null;
+
+            //(van, index) = EgyszeruProgTetelek.LinearisKeresesTetel(tomb, labda => tomb[labda] % 9 == 0); //fontos az igaz állítás, mivel a feltétel !P(x[i])
+            
+            ////var ertek = EgyszeruProgTetelek.LinearisKeresesTetel(tomb, labda => tomb[labda] % 9 == 0); //szerintem átláthatóbb (egy bizonyos szintig)
+
+            //Console.WriteLine($"Van ilyen elem: {van}\nKeresett eleme indexe: {index}");
 
             #endregion
 
 
-            int[] tombA = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            int[] tombB = { 1, 2, 8, 9, 10, 11, 12 };
+            int[] tombA = { 1, 2, 3, 4, 5, 10, 9, 8, 7, 6 };
 
-
+            
 
 
 
---
+++ SzigoHelperConsole/Rendezesek.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzigoHelperConsole
+{
+    public class Rendezesek
+    {
+        public static void EgyszeruCseresRendezesTetel(ref int[] tomb)
+        {
+            for (int i = 0; i < tomb.Length - 1; i++)
+            {
+                for (int j = i + 1; j < tomb.Length; j++)
+                {
+                    if (tomb[i] > tomb[j]) //relációs jel megfordítása esetén, csökkenő sorrend
+                    {
+                        var temp = tomb[i];
+                        tomb[i] = tomb[j];
+                        tomb[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void MinimumCseresRendezesTelelt(ref int[] tomb)
+        {
+            int db = 0;
+            for (int i = 0; i < tomb.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < tomb.Length; j++)
+                {
+                    db++;
+                    if (tomb[min] > tomb[j])
+                    {
+                        min = j;
+                    }
+                }
+                var temp = tomb[i];
+                tomb[i] = tomb[min];
+                tomb[min] = temp;
+            }
+            Console.WriteLine($"Összehasonlítások száma: {db}");
+        }
+
+        public static void BuborekRendezesTetel(ref int[] tomb)
+        {
+            int db = 0;
+            for (int i = tomb.Length; i > 1; i--)
+            {
+                for (int j = 0; j < i - 1; j++)
+                {
+                    db++;
+                    if (tomb[j] > tomb[j + 1])
+                    {
+                        var temp = tomb[j];
+                        tomb[j] = tomb[j + 1];
+                        tomb[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine($"Összehasonlítások száma: {db}");
+
+        }
+
+        public static void JavitottBuborekRendezesTetel(ref int[] tomb)
+        {
+            int i = tomb.Length;
+            int db = 0;
+            while (i >= 1)
+            {
+                int index = 0;
+                for (int j = 0; j < i - 1; j++)
+                {
+                    db++;
+                    if (tomb[j] > tomb[j + 1])
+                    {
+                        var temp = tomb[j];
+                        tomb[j] = tomb[j + 1];
+                        tomb[j + 1] = temp;
+                        index = j + 1;
+                    }
+                }
+                i = index;
+            }
+            Console.WriteLine($"Összehasonlítások száma: {db}");
+        }
+
+        public static void BeillesztésesRendezésTetel(ref int[] tomb)
+        {
+            int db = 0;
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                int j = i - 1;
+                db++;
+                while (j >= 0 && (tomb[j] > tomb[j + 1]))
+                {
+                    var temp = tomb[j];
+                    tomb[j] = tomb[j + 1];
+                    tomb[j + 1] = temp;
+                    j--;
+                }
+            }
+            Console.WriteLine($"Összehasonlítások száma: {db}");
+
+        }
+
+        public static void JavitottBeillesztésesRendezesTetel(ref int[] tomb)
+        {
+            int db = 0;
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                int j = i - 1;
+                int seged = tomb[i];
+
+                db++;
+                while (j >= 0 && tomb[j] > seged)
+                {
+                    tomb[j + 1] = tomb[j];
+                    j--;
+                }
+                tomb[j + 1] = seged;
+            }
+            Console.WriteLine($"Összehasonlítások száma: {db}");
+
+        }
+
+    }
+}
