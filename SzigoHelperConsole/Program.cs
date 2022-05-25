9d83a8e9c0aa364f46f7f32c7dc7caadf849c284
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-05-25 22:44:22 +0200

    Samples for understanding

--- SzigoHelperConsole/Program.cs
+++ SzigoHelperConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SzigoHelperConsole
 {
@@ -6,22 +7,51 @@ namespace SzigoHelperConsole
     {
         static void Main(string[] args)
         {
-            //segítség Turpe-höz, és lambdához
-            //ha rosszul van megírva a feltétel, akkor rossz eredmény várható
-
-            //int[] tomb = { 1, 2, 3, 4, 9, 8, 7, 6, 5, 10 };
-
-            //var a = EgyszeruProgTetelek.LinearisKeresesTetel(tomb, labda => tomb[labda] % 9 == 0); //fontos az igaz állítás, mivel a feltétel !P(x[i])
-
-            //Console.WriteLine(a);
-
-            //Rekurzio.Hanoi(7, "A", "C" ,"B");
+            //Mintak();
 
 
-
-
-
+            
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Egy tömböt feltölt véletlenszerű számokkal
+        /// </summary>
+        /// <param name="tomb">Feltöltendő tömb</param>
+        /// <param name="darabSzam">Véletlenszerű számok darabszáma</param>
+        /// <param name="alsoHatar">Legkisebb véletlen szám</param>
+        /// <param name="felsoHatar">Legnagyobb véletlen szám</param>
+        static void TombFeltoltRandom(ref int[] tomb, int darabSzam, int alsoHatar, int felsoHatar)
+        {
+            tomb = new int[darabSzam];
+            Random rand = new Random();
+            for (int i = 0; i < darabSzam; i++)
+            {
+                tomb[i] = rand.Next(alsoHatar, felsoHatar + 1);
+            }
+        }
+
+        static void Mintak()
+        {
+            int[] tombA = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //páros számok a tömb elejére
+            OsszetettProgTetelek.SzetvalogatasHelybenCsereNelkul(ref tombA, lambda => tombA[lambda] % 2 == 0);
+
+
+            string[][] tombBackTrack =
+            {
+                new string[]{"Miklós", "Klaudia"},
+                new string[]{"Miklós", "András"},
+                new string[]{"András", "Zsolt"},
+                new string[]{"Géza", "Zsolt", "Palika"},
+                new string[]{"Géza", "András"},
+                new string[]{"Miklós", "Géza"}
+            };
+
+            bool ok = false;
+            string[] kimenet = new string[tombBackTrack.Length];
+            Optimalizalas.VisszaLepesesKereses<string>(0, tombBackTrack, kimenet, ref ok);
         }
     }
 }
