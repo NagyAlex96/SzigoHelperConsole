985aa8a14c7a18760a677e44d4ac0b622d6470f7
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-05-25 15:15:38 +0200

    Rekurzio fixes

--- SzigoHelperConsole/Program.cs
+++ SzigoHelperConsole/Program.cs
@@ -6,29 +6,20 @@ namespace SzigoHelperConsole
     {
         static void Main(string[] args)
         {
-            #region Segítség a Turple-höz és a lambdához
+            //segítség Turpe-höz, és lambdához
+            //ha rosszul van megírva a feltétel, akkor rossz eredmény várható
 
             //int[] tomb = { 1, 2, 3, 4, 9, 8, 7, 6, 5, 10 };
 
-            //bool van = false;
-            //int? index = null;
+            //var a = EgyszeruProgTetelek.LinearisKeresesTetel(tomb, labda => tomb[labda] % 9 == 0); //fontos az igaz állítás, mivel a feltétel !P(x[i])
 
-            //(van, index) = EgyszeruProgTetelek.LinearisKeresesTetel(tomb, labda => tomb[labda] % 9 == 0); //fontos az igaz állítás, mivel a feltétel !P(x[i])
-            
-            ////var ertek = EgyszeruProgTetelek.LinearisKeresesTetel(tomb, labda => tomb[labda] % 9 == 0); //szerintem átláthatóbb (egy bizonyos szintig)
+            //Console.WriteLine(a);
 
-            //Console.WriteLine($"Van ilyen elem: {van}\nKeresett eleme indexe: {index}");
-
-            #endregion
-
-
-            int[] tombA = { 1, 2, 3, 4, 5, 10, 9, 8, 7, 6 };
-
-            
+            //Rekurzio.Hanoi(7, "A", "C" ,"B");
 
 
 
-            ;
+
 
             Console.ReadKey();
         }
--- SzigoHelperConsole/Rekurzio.cs
+++ SzigoHelperConsole/Rekurzio.cs
@@ -8,64 +8,168 @@ namespace SzigoHelperConsole
 {
     public class Rekurzio
     {
-        public static Int64 FaktorialisRekurzivTetel(Int64 egesz)
+        /// <summary>
+        /// N darab pozitív egész szám szorzata
+        /// </summary>
+        /// <param name="N">Természetes szám, melynek a faktoriálisát meg kívánjuk határozni</param>
+        /// <returns>N! értéke</returns>
+        public static int FaktorialisIterativan(int N)
         {
-            if (egesz == 0)
+            int ertek = 1;
+
+            for (int i = 2; i <= N; i++)
+            {
+                ertek *= i;
+            }
+
+            return ertek;
+        }
+
+        /// <summary>
+        /// N darab pozitív egész szám szorzata
+        /// </summary>
+        /// <param name="N">Természetes szám, amelynek faktoriálisát meg szeretnék határozni</param>
+        /// <returns>N! értéke</returns>
+        public static int FaktorialisRekurzivTetel(int N)
+        {
+            if (N == 0)
             {
                 return 1;
             }
             else
             {
-                return egesz * FaktorialisRekurzivTetel(egesz - 1);
+                return N * FaktorialisRekurzivTetel(N - 1);
             }
         }
 
-        public static Int64 FibonacciRekurzivTetel(Int64 egesz)
+        /// <summary>
+        /// Fibonacci szám meghatározása
+        /// </summary>
+        /// <param name="N">Nemnegatív egész, mely megadja, hogy hányadik Fibonacci számot akarjuk meghatározni</param>
+        /// <returns>N-edik Fibonacci szám értéke</returns>
+        public static int FibonacciIterativan(int N)
         {
-            if (egesz <= 2)
+            int aktualis = 1;
+            int elozo = 1;
+
+            for (int i = 1; i < N - 1; i++)
+            {
+                int atmeneti = aktualis + elozo;
+                elozo = aktualis;
+                aktualis = atmeneti;
+            }
+            return aktualis;
+        }
+
+        /// <summary>
+        /// Fibonacci szám meghatározása
+        /// </summary>
+        /// <param name="N">Nemnegatív egész, mely megadja, hogy hányadik Fibonacci számot akarjuk meghatározni</param>
+        /// <returns>N-edik Fibonacci szám értéke</returns>
+        public static int FibonacciRekurzivTetel(int N)
+        {
+            if (N <= 2)
             {
                 return 1;
             }
             else
             {
-                return FibonacciRekurzivTetel(egesz - 2) + FibonacciRekurzivTetel(egesz - 1);
+                return FibonacciRekurzivTetel(N - 2) + FibonacciRekurzivTetel(N - 1);
             }
         }
 
-        public static Int64 HatvanyozasRekurzivTetel(Int64 egesz, Int64 hatvanyKitevo)
+        /// <summary>
+        /// Valós szám, pozitív egész kitevős hatványának meghatározása
+        /// </summary>
+        /// <param name="a">Pozitív valós szám. Hatványalap</param>
+        /// <param name="N">Pozitív valós szám. Hatványkitevő</param>
+        /// <returns>A^n értéke</returns>
+        public static int HatvanyozasIterativan(int a, int N)
         {
-            if (hatvanyKitevo == 1)
+            int ertek = a;
+
+            for (int i = 1; i < N; i++)
             {
-                return egesz;
+                ertek *= a;
+            }
+            return ertek;
+        }
+
+        /// <summary>
+        /// Valós szám, pozitív egész kitevős hatványának meghatározása
+        /// </summary>
+        /// <param name="a">Pozitív valós szám. Hatványalap</param>
+        /// <param name="N">Pozitív valós szám. Hatványkitevő</param>
+        /// <returns>A^n értéke</returns>
+        public static int HatvanyozasRekurzivTetel(int a, int N)
+        {
+            if (N == 1)
+            {
+                return a;
             }
             else
             {
-                return egesz * HatvanyozasRekurzivTetel(egesz, hatvanyKitevo - 1);
+                return a * HatvanyozasRekurzivTetel(a, N - 1);
             }
         }
 
-        public static Int64 HatvanyFelezoRekurzivTetel(Int64 egesz, Int64 hatvanyKitevo)
+        /// <summary>
+        /// Valós szám, pozitív egész kitevős hatványának meghatározása
+        /// </summary>
+        /// <param name="a">Pozitív valós szám. Hatványalap</param>
+        /// <param name="N">Pozitív valós szám. Hatványkitevő</param>
+        /// <returns>A^n értéke</returns>
+        public static Int64 HatvanyFelezoRekurzivTetel(int a, int N)
         {
-            if (hatvanyKitevo == 1)
+            if (N == 1)
             {
-                return egesz;
+                return a;
             }
             else
             {
-                if (hatvanyKitevo % 2 == 0)
+                if (N % 2 == 0)
                 {
-                    Int64 seged = HatvanyFelezoRekurzivTetel(egesz, hatvanyKitevo / 2);
+                    Int64 seged = HatvanyFelezoRekurzivTetel(a, N / 2);
                     return seged * seged;
                 }
                 else
                 {
-                    Int64 seged = HatvanyFelezoRekurzivTetel(egesz, (hatvanyKitevo - 1) / 2);
-                    return egesz * seged * seged;
+                    Int64 seged = HatvanyFelezoRekurzivTetel(a, (N - 1) / 2);
+                    return a * seged * seged;
                 }
             }
         }
 
-        public static int SorozatSzamitasRekurzivTetel(int[] bemenetiTomb, int jobb)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="N">A mozgatandó korongok száma</param>
+        /// <param name="forras">Az a rúd, amelyről mozgatni akarunk</param>
+        /// <param name="cel">Az a rúd, amelyre mozgatni akarunk</param>
+        /// <param name="seged">Az a rúd, amelyet segítségül használhatunk a mozgatáshoz</param>
+        public static void Hanoi(int N, string forras, string cel, string seged)
+        {
+            if(N == 1)
+            {
+                Console.WriteLine($"Mozgassuk: {N}(as/es) korongot {forras}(ról/ről), {cel}(ra/re)");
+                //pszeudóban Mozgat van, Nálam kiíratás (átláthatóbb és a lényegen nem változtat)
+            }
+            else
+            {
+                Hanoi(N - 1, forras, seged, cel);
+                Console.WriteLine($"Mozgassuk: {N}(as/es) korongot {forras}(ról/ről), {cel}(ra/re)");
+                //pszeudóban Mozgat van, Nálam kiíratás (átláthatóbb és a lényegen nem változtat)
+                Hanoi(N-1, seged, cel, forras);
+            }
+        }
+
+        /// <summary>
+        /// Tömb összes eleme között elvégez egy megadott műveletet (összeadás jelenleg)
+        /// </summary>
+        /// <param name="inputArray">Feldolgozandó tömb</param>
+        /// <param name="jobb">Tömb első elemétől a jobb indexű eleméig tartó résztömböt dolgozza fel</param>
+        /// <returns></returns>
+        public static int SorozatSzamitasRekurzivTetel(int[] inputArray, int jobb)
         {
             if (jobb == -1)
             {
@@ -73,30 +177,44 @@ namespace SzigoHelperConsole
             }
             else
             {
-                return SorozatSzamitasRekurzivTetel(bemenetiTomb, jobb - 1) + bemenetiTomb[jobb];
+                return SorozatSzamitasRekurzivTetel(inputArray, jobb - 1) + inputArray[jobb];
             }
         }
 
-        public static int LinearisKeresesRekurzivTetel(int[] bemenetiTomb, int bal)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputArray">A feldolgozandó tömb</param>
+        /// <param name="bal">A bemeneti tömbnek a bal indexű elemétől az utolsó eleméig tartó résztömböt dolgozza fel</param>
+        /// <param name="P">Logikai értéket visszaadó tulajdonságfüggvény</param>
+        /// <returns></returns>
+        public static int? LinearisKeresesRekurzivTetel(int[] inputArray, int bal, Func<int, bool> P)
         {
-            if (bal >= bemenetiTomb.Length)
+            if (bal >= inputArray.Length)
             {
-                return 0;
+                return null;
             }
             else
             {
-                if (bemenetiTomb[bal] % 2 == 0)
+                if (P(bal))
                 {
                     return bal;
                 }
                 else
                 {
-                    return LinearisKeresesRekurzivTetel(bemenetiTomb, bal + 1);
+                    return LinearisKeresesRekurzivTetel(inputArray, bal + 1, P);
                 }
             }
         }
 
-        public static int MegszamlalasRekurzivTetel(int[] bemenetiTomb, int jobb)
+        /// <summary>
+        /// Megszámolja a tömbben a P tulajdonságú elemek darabszámát
+        /// </summary>
+        /// <param name="inputArray">A feldolgozandó tömb</param>
+        /// <param name="jobb">A tömbnek az első elemétől a jobb indexű eleméig tartó résztömböt dolgozza fel</param>
+        /// <param name="P">Logikai értéket visszaadó tulajdonság függvény</param>
+        /// <returns>P tulajdonságú elemek darabszáma</returns>
+        public static int MegszamlalasRekurzivTetel(int[] inputArray, int jobb, Func<int, bool> P)
         {
             if (jobb == -1)
             {
@@ -104,27 +222,33 @@ namespace SzigoHelperConsole
             }
             else
             {
-                if (bemenetiTomb[jobb] % 2 == 0)
+                if (P(jobb))
                 {
-                    return 1 + MegszamlalasRekurzivTetel(bemenetiTomb, jobb - 1);
+                    return 1 + MegszamlalasRekurzivTetel(inputArray, jobb - 1, P);
                 }
                 else
                 {
-                    return MegszamlalasRekurzivTetel(bemenetiTomb, jobb - 1);
+                    return MegszamlalasRekurzivTetel(inputArray, jobb - 1, P);
                 }
             }
         }
 
-        public static int MaximumKivalasztasRekurzivTetel(int[] bemenetiTomb, int jobb)
+        /// <summary>
+        /// Egy tömb elemei közül megadja a maximális értékű elem indexét
+        /// </summary>
+        /// <param name="inputArray">A feldolgozandó tömb</param>
+        /// <param name="jobb">A tömbnek az első elemétől a jobb indexű eleméig tartó résztömböt dolgozza fel</param>
+        /// <returns>Maximális értékű elem indexe</returns>
+        public static int MaximumKivalasztasRekurzivTetel(int[] inputArray, int jobb)
         {
             if (jobb == 0)
             {
-                return 1;
+                return 0;
             }
             else
             {
-                int max = MaximumKivalasztasRekurzivTetel(bemenetiTomb, jobb - 1);
-                if (bemenetiTomb[jobb] > max)
+                int max = MaximumKivalasztasRekurzivTetel(inputArray, jobb - 1);
+                if (inputArray[jobb] > inputArray[max])
                 {
                     return jobb;
                 }
