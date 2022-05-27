e37643693c5a6f70334ab84ae38d4f9c66d05475
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-05-27 17:25:51 +0200

    Gráf alap

---
+++ SzigoHelperConsole/Szoft2/Grafok/GrafSzomszedsagiLista.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzigoHelperConsole.Szoft2.Grafok
+{
+    public class GrafSzomszedsagiLista<T> : Graf<T>
+    {
+        List<T> tartalmak; //csúcsok
+        List<List<El>> szomszedok; //élek
+
+        public GrafSzomszedsagiLista()
+        {
+            tartalmak = new List<T>();
+            szomszedok = new List<List<El>>();
+        }
+
+        protected override int CsucsokSzama
+        {
+            get
+            {
+                return tartalmak.Count;
+            }
+        }
+
+        public override void UjCsucs(T tartalom)
+        {
+            tartalmak.Add(tartalom);
+            szomszedok.Add(new List<El>());
+        }
+
+        public override void UjEl(T from, T to, double weight = 1, bool isDirected = false)
+        {
+            int index = tartalmak.IndexOf(from);
+            szomszedok[index].Add(new Graf<T>.El()
+            {
+                hova = to,
+                suly = weight,
+            });
+
+            if (!isDirected) //irányítattlan a gráf 
+            {
+                index = tartalmak.IndexOf(to);
+                szomszedok[index].Add(new Graf<T>.El()
+                {
+                    hova = from,
+                    suly = weight,
+                });
+            }
+
+
+        }
+
+        protected override int AdottCsucsIndexe(T csucs)
+        {
+            return tartalmak.IndexOf(csucs);
+        }
+
+        protected override T AdottIndexuCsucs(int index)
+        {
+            return tartalmak[index];
+        }
+
+        protected override List<El> Szomszedok(T csucs)
+        {
+            int index = tartalmak.IndexOf(csucs);
+
+            return szomszedok[index];
+        }
+
+    }
+}
